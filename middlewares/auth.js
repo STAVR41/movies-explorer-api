a35cb86d45188a15de6ba278fcc960b14af904b0
Pavel Shershnev <pavelsersnev@gmail.com>
2023-09-16 21:14:32 +1200

    FIX

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,11 +5,10 @@ const { JWT_SECRET_KEY } = require('../utils/constants');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
+  const token = req.cookies.jwt;
+  if (!token) {
     return next(new UnauthorizedError('Необходима авторизация'));
   }
-  const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_KEY);
